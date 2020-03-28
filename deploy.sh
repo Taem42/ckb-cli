44295da337335ae2d5102783baef26a11c02f29c
keroro520 <keroroxx520@gmail.com>
2020-03-29 05:48:53 +0800

    finish

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+
+./target/release/ckb-cli --wait-for-sync wallet transfer --privkey-path /Users/apple/ckb-testing/bank0 --to-address ckt1qyqvsv5240xeh85wvnau2eky8pwrhh4jr8ts8vyj37 --capacity 10000000 --tx-fee 1 --to-data-path /Users/apple/cryptape/ckb-cli/spec/cells/forty
